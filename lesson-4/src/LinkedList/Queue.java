fcf9343317e19f3e4860c9a0c2c67568b3dbcff3
Semyon <khalyavin.semyon@yandex.ru>
2020-04-13 10:40:56 +0300

    Задание по спискам выполнено

---
+++ lesson-4/src/LinkedList/Queue.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class Queue{
+    private LinkedList queue;
+
+    public Queue(){
+        queue = new LinkedList();
+    }
+
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+
+    public void insert(String name, int age){
+        queue.insert(name, age);
+    }
+
+    public Link delete(){
+        return queue.delete();
+    }
+
+    public void display(){
+        queue.display();
+    }
+}
