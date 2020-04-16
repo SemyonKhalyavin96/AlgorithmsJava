c66bf3415b530428562f7fd6eeac4c731cfacfce
Semyon <khalyavin.semyon@yandex.ru>
2020-04-16 14:33:54 +0300

    Начальный проект игры

---
+++ Game/src/Doctor.java
@@ -0,0 +1,16 @@
+public class Doctor extends Hero {
+
+    public Doctor(int heal, String name, int damage, int addHeal) {
+        super(heal, name, damage, addHeal);
+    }
+
+    @Override
+    void hit(Hero hero) {
+        System.out.println("Доктор не может бить!");
+    }
+
+    @Override
+    void healing(Hero hero) {
+        hero.addHealth(addHeal);
+    }
+}
